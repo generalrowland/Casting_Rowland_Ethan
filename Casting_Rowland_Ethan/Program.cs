328d53457e27b79ea7e882e65c0ad2bceec6d471
generalrowland <ethandale0916@gmail.com>
2023-02-27 13:47:15 -0600

    update

--- Casting_Rowland_Ethan/Program.cs
+++ Casting_Rowland_Ethan/Program.cs
@@ -6,7 +6,20 @@ namespace Casting_Rowland_Ethan
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Asks user to enter a whole number and assigns input to integer
+            Console.WriteLine("Please enter a whole number!");
+            int myFavNum = Convert.ToInt32(Console.ReadLine());
+
+            // Prints favorite number to console
+            Console.WriteLine("Your favorite is " + myFavNum);
+
+            // Asks user a true/false question and assigns response to a boolean
+            Console.WriteLine("Do you like video games? (Please enter 'true' or 'false')");
+            bool isTrue = Convert.ToBoolean(Console.ReadLine());
+
+            // Print the result of question asked
+            Console.WriteLine($"It is {isTrue} that I like video games!");
+
         }
     }
 }
